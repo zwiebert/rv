d9ac8dfd794fce58bfbe73c2ab3ad98a97eb9939
zwiebert <bertw@online.de>
2020-07-18 14:46:27 +0200

    Remove hot reload attempt

--- esp32/netmcu/components/http_server_content/webapp/njs/rollup.config.js
+++ esp32/netmcu/components/http_server_content/webapp/njs/rollup.config.js
@@ -3,15 +3,13 @@ import json from '@rollup/plugin-json';
 import { terser } from 'rollup-plugin-terser';
 import strip from '@rollup/plugin-strip';
 import { eslint } from "rollup-plugin-eslint";
-import svelte from 'rollup-plugin-svelte-hot';
+import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import sveltePreprocess from 'svelte-preprocess';
 import { watch } from 'rollup';
 
 export const isProduction = process.env.buildTarget === "PROD";
 const useTailwind = process.env.useTailwind === "yes";
-
-let hot = !isProduction && !!watch;
 
 export default {
   onwarn(warning, rollupWarn) {
@@ -52,63 +50,9 @@ export default {
     eslint(),
     svelte({
 
-      // Use `hot: true` to use default options (as follow).
-      //
-      // Set `hot: false` to completely disable HMR shenanigans (you need this
-      // for `npm run build`, for example).
-      //
-      hot: hot && {
-        // Prevent preserving local component state
-        noPreserveState: false,
-
-        // If this string appears anywhere in your component's code, then local
-        // state won't be preserved, even when noPreserveState is false
-        noPreserveStateKey: '@!hmr',
-
-        // Prevent doing a full reload on next HMR update after fatal error
-        noReload: false,
-
-        // Try to recover after runtime errors in component init
-        optimistic: false,
-
-        // --- Advanced ---
-
-        // By default, when the hot option is enabled, the `css` option of this
-        // plugin (same option as official plugin) will be changed to `false`,
-        // because extracting CSS doesn't work with HMR currently. You can use
-        // this option to disable this behaviour if it cause problems with your
-        // setup.
-        noDisableCss: true,
-
-        // When you change only the <style> part of a component, then only the
-        // CSS will be reinjected. Existing component instances won't be
-        // recreated. Set `false` to force recreation.
-        injectCss: true,
-
-        // Delay to mitigate FOUC (flash of unstyled content) when a component
-        // is updated, before the new version with the new CSS is loaded.
-        cssEjectDelay: 100,
-
-        // Prevent adding an HMR accept handler to components with
-        // accessors option to true, or to components with named exports
-        // (from <script context="module">). This have the effect of
-        // recreating the consumer of those components, instead of the
-        // component themselves, on HMR updates. This might be needed to
-        // reflect changes to accessors / named exports in the parents,
-        // depending on how you use them.
-        acceptAccessors: false,
-        acceptNamedExports: false,
-
-        // Set true to enable support for Nollup (note: when not specified, this
-        // is automatically detected based on the NOLLUP env variable)
-        nollup: false,
-      },
-
       dev: !isProduction,
       //emitCss: true,
-      ...(true && {
-      css: css => { css.write(isProduction ? 'build/wapp.css' : 'build_dev/wapp.css'); }
-      }),
+      css: css => { css.write(isProduction ? 'build/wapp.css' : 'build_dev/wapp.css'); },
       onwarn: (warning, handler) => {
         // e.g. don't warn on <marquee> elements, cos they're cool
         if (warning.code === 'a11y-no-onchange') return;
@@ -130,22 +74,3 @@ export default {
     })
   ]
 };
-
-
-
-function serve() {
-	let started = false;
-
-	return {
-		writeBundle() {
-			if (!started) {
-				started = true;
-
-				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-					stdio: ['ignore', 'inherit', 'inherit'],
-					shell: true
-				});
-			}
-		}
-	};
-}
