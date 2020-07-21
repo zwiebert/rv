a07301ea380ecb74af8cbc92bc4a78fc49ce62e3
zwiebert <bertw@online.de>
2020-07-22 01:43:28 +0200

    Clean up code

--- esp32/netmcu/components/webapp/njs/src/main.svelte
+++ esp32/netmcu/components/webapp/njs/src/main.svelte
@@ -2,9 +2,7 @@
   "use strict";
   import Layout from "./layout.svelte";
   import Wapp from "./wapp.svelte";
-
-  export let isProduction = false;
 </script>
 
 <Layout />
-<Wapp {isProduction} />
+<Wapp  />
