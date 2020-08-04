f478e56a04fd17fd10b89dd278790a9e73ba88af
zwiebert <bertw@online.de>
2020-08-04 13:11:42 +0200

    Send commands to STM32 as JSON only

--- esp32/netmcu/components/cli_app/parm_cmd.c
+++ esp32/netmcu/components/cli_app/parm_cmd.c
@@ -27,7 +27,6 @@ SemaphoreHandle_t uart_mutex;
 #define D(x)
 #endif
 
-void cliCmd_waitForResponse();
 volatile bool cli_cmd_wait_for_response;
 
 #define ONE_MINUTE (60)
@@ -42,21 +41,21 @@ const char cli_help_parmCmd[]  =
 
 #define KEY_DURATION_PREFIX "dur"
 #define KEY_DURATION_PREFIX_LEN ((sizeof KEY_DURATION_PREFIX) - 1)
-#define CMD_ASK_DURATIONS " dur=?"
+#define CMD_ASK_DURATIONS "\"dur\":\"?\","
 #define CMD_ASK_DURATIONS_LEN (sizeof CMD_ASK_DURATIONS - 1)
 
 #define KEY_REMAINING_PREFIX "rem"
 #define KEY_REMAINING_TIME_PREFIX_LEN ((sizeof KEY_REMAINING_TIME_PREFIX) - 1)
-#define CMD_ASK_REMAINING_TIMES " rem=?"
+#define CMD_ASK_REMAINING_TIMES "\"rem\":\"?\","
 #define CMD_ASK_REMAINING_TIMES_LEN (sizeof CMD_ASK_REMAINING_TIMES - 1)
 
 #define KEY_STATUS_PREFIX "status"
 #define KEY_STATUS_PREFIX_LEN ((sizeof KEY_STATUS_PREFIX) - 1)
-#define CMD_ASK_STATUS " status=?"
+#define CMD_ASK_STATUS "\"status\":\"?\","
 #define CMD_ASK_STATUS_LEN (sizeof CMD_ASK_STATUS - 1)
 
 #define RV_VERSION "rv-version"
-#define CMD_ASK_VERSION " version=?"
+#define CMD_ASK_VERSION "\"version\":\"?\","
 
 #define BUF_SIZE 128
 #define ZONE_COUNT 14
@@ -65,11 +64,10 @@ const char cli_help_parmCmd[]  =
 int
 process_parmCmd(clpar p[], int len) {
   int arg_idx;
-  char buf[BUF_SIZE] = "cmd";
+  char buf[BUF_SIZE] = "{\"cmd\":{";
   int buf_idx = sizeof(buf);
 
   bool hasCmdLine = false;
-  bool reqResponse = false;
 
   for (arg_idx = 1; arg_idx < len; ++arg_idx) {
     const char *key = p[arg_idx].key, *val = p[arg_idx].val;
@@ -80,21 +78,21 @@ process_parmCmd(clpar p[], int len) {
 
     } else if (strcmp(key, "dur") == 0 && *val == '?') {
       strcat(buf, CMD_ASK_DURATIONS);
-      reqResponse = true;
+
       hasCmdLine = true;
 
     } else if (strcmp(key, RV_VERSION) == 0 && *val == '?') {
       strcat(buf, CMD_ASK_VERSION);
-      reqResponse = true;
+
       hasCmdLine = true;
 
     } else if (strcmp(key, "rem") == 0 && *val == '?') {
       strcat(buf, CMD_ASK_REMAINING_TIMES);
-      reqResponse = true;
+
       hasCmdLine = true;
     } else if (strcmp(key, "status") == 0 && *val == '?') {
       strcat(buf, CMD_ASK_STATUS);
-      reqResponse = true;
+
       hasCmdLine = true;
     } else if (strncmp(key, KEY_DURATION_PREFIX, KEY_DURATION_PREFIX_LEN) == 0) {
       int zone=-1, timer_number=0;
@@ -109,7 +107,7 @@ process_parmCmd(clpar p[], int len) {
 
         sscanf(val, "%f,%d,%f,%d,%f,%d,%f,%f", &on, &ignoreRainSensor, &off, &repeats, &period, &dInterval, &dhBegin, &dhEnd);
 
-        buf_idx += sprintf(buf + strlen(buf), " dur%d.%d=%d,%d,%d,%d,%d,%d,%d,%d", zone, timer_number, (int) (on * ONE_MINUTE), ignoreRainSensor, (int) (off * ONE_MINUTE), repeats,
+        buf_idx += sprintf(buf + strlen(buf), "\"dur%d.%d\":\"%d,%d,%d,%d,%d,%d,%d,%d\",", zone, timer_number, (int) (on * ONE_MINUTE), ignoreRainSensor, (int) (off * ONE_MINUTE), repeats,
             (int) (period * ONE_HOUR), dInterval, (int)(dhBegin * ONE_HOUR), (int)(dhEnd * ONE_HOUR));
 
         hasCmdLine = true;
@@ -117,7 +115,7 @@ process_parmCmd(clpar p[], int len) {
         float duration = 0;
         sscanf(val, "%f", &duration);
         if (0 <= zone && zone < ZONE_COUNT && 0 <= duration && duration <= MAX_DURATION) {
-          buf_idx += sprintf(buf + strlen(buf), " dur%d.%d=%d", zone, timer_number, (int)(duration * 60));
+          buf_idx += sprintf(buf + strlen(buf), "\"dur%d.%d\":%d", zone, timer_number, (int)(duration * 60));
           hasCmdLine = true;
         } else {
           db_printf("error: out of range");
@@ -129,18 +127,11 @@ process_parmCmd(clpar p[], int len) {
   }
 
   if (hasCmdLine) {
-    strcat(buf, ";\n");
+    buf[strlen(buf)-1] = '\0';
+    strcat(buf, "}};\n");
     dbg_vpf(db_printf("cmd2stm32: <%s>\n", buf));
     if (xSemaphoreTakeRecursive(uart_mutex, portMAX_DELAY)) {
       stm32_write(buf, strlen(buf));
-
-      if (reqResponse) {
-#ifdef USE_STM32COM
-     // TODO
-#else
-        cliCmd_waitForResponse();
-#endif
-      }
       xSemaphoreGiveRecursive(uart_mutex);
     }
   }
@@ -148,47 +139,6 @@ process_parmCmd(clpar p[], int len) {
   return 0;
 }
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
-
-// wait for response
-void cliCmd_waitForResponse() {
-#define WFR_TOTAL_MS 2000
-#define WFR_INTERVAL_MS 50
-
-  const int buf_size = 80;
-  char buf[buf_size];
-
-  int n = 0;
-  D(db_printf("wait for response\n"));
-  for (int i = 0; i < (WFR_TOTAL_MS / WFR_INTERVAL_MS); ++i) {
-    vTaskDelay(WFR_INTERVAL_MS / portTICK_PERIOD_MS);
-    D(db_printf(":"));
-    n = stm32_read(buf, buf_size - 1);
-    if (n > 0) {
-      buf[n] = '\0';
-      char *sc = strchr(buf, '\n');
-      if (sc) {
-        *sc = ',';
-        sc[1] = '\0';
-      }
-      sj_cat_to_buf(buf);
-      D(db_printf("cli_cmd(from stm32): <%s>\n", buf));
-      if (sc) {
-        *sc = '\0';
-        break;
-      }
-    }
-
-  }
-  D(db_printf("json: <%s>\n", sj_get_json()));
-  D(db_printf("stop wait for response\n"));
-
-}
-
-#define STM32_STATUS "status "
-#define STM32_STATUS_LEN (sizeof (STM32_STATUS) - 1)
 
 
 
