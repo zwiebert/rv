bd852e7b772c6b68ef1e32fa9e8c44f1b8b4cd1b
zwiebert <bertw@online.de>
2024-03-17 16:58:10 +0100

    Improve code

--- esp32/netmcu/components/rv_netmcu/full_auto/fa_weather.cc
+++ esp32/netmcu/components/rv_netmcu/full_auto/fa_weather.cc
@@ -16,17 +16,43 @@ bool fa_poll_weather_full_hour() {
 #include <full_auto/single_valve.hh>
 
 int FaContentReader::open(const char *name, const char *query) {
-  return 1;
+  int fd = 0;
+  auto &fda = m_file_data[fd];
+  if (fda.is_open)
+    return -1;
+
+  fda.is_open = true;
+
+  return fd;
 }
 int FaContentReader::read(int fd, char *buf, unsigned buf_size) {
+  if (fd != 0)
+    return -1;
+  auto &fda = m_file_data[fd];
+
+  if (!fda.is_open)
+    return -1;
+
+  if (fda.bytes_read)
+    return 0; // EOF
+
   SingleValve sv;
-  if (sv.to_json(buf, buf_size))
-    return strlen(buf);
+  if (sv.to_json(buf, buf_size)) {
+    fda.bytes_read = strlen(buf);
+    return fda.bytes_read;
+  }
 
   return -1;
 }
 int FaContentReader::close(int fd) {
-  return 0;
+  if (fd != 0)
+    return -1;
+  auto &fda = m_file_data[fd];
 
+  if (!fda.is_open)
+    return -1;
+
+  fda = FileData();
+  return 0;
 }
 
--- esp32/netmcu/components/rv_netmcu/full_auto/include/full_auto/weather.hh
+++ esp32/netmcu/components/rv_netmcu/full_auto/include/full_auto/weather.hh
@@ -12,6 +12,12 @@ public:
   virtual int open(const char *name, const char *query);
   virtual int read(int fd, char *buf, unsigned buf_size);
   virtual int close(int fd);
+private:
+  static constexpr unsigned OUR_NMB_FILES = 1;
+  struct FileData {
+  int bytes_read = 0;
+  bool is_open = false; // FIXME: only one open file allowed for now
+  } m_file_data[OUR_NMB_FILES];
 };
 
 
