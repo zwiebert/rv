8bfbe5ae0942cbcdeb6f6a1b468428343444dfcc
zwiebert <bertw@online.de>
2024-03-23 08:46:52 +0100

    Update nodejs packages

--- esp32/netmcu/components/rv_netmcu/app_http_server/esp32/uri_handlers.cc
+++ esp32/netmcu/components/rv_netmcu/app_http_server/esp32/uri_handlers.cc
@@ -31,22 +31,33 @@ static const char *logtag="http_handler";
 
 
 ////////////////////////// URI handlers /////////////////////////////////
+
 static int response_callback(httpd_req_t *req, const char *src, ssize_t src_len, int chunk_status) {
   if (src_len < 0)
     src_len = strlen(src);
 
-  D(ESP_LOGW(logtag, "writer for /cmd.json: is_final=%d src_ptr=%p, src_len=%d src=<%*s>",
-          is_final, src, src_len, src_len, src));
+  //const bool first_chunk = chunk_status == 0 || chunk_status == -1;
+  const bool single_chunk = chunk_status == 0;
+  const bool final_chunk = 0 <= chunk_status; // 0 or total number of chunks
+
+  D(ESP_LOGW(logtag, "writer for /cmd.json: chunk_status=%d src_ptr=%p, src_len=%d src=<%*s>",
+          chunk_status, src, src_len, src_len, src));
+
+  // send in one go
+  if (single_chunk) {
+    if (!(ESP_OK == httpd_resp_set_type(req, "application/json") && ESP_OK == httpd_resp_send(req, src, src_len)))
+      return -1;
+    return src_len;
+  }
+
   // send chunks. last chunk needs to have size zero
   if (!(ESP_OK == httpd_resp_set_type(req, "application/json") && ESP_OK == httpd_resp_send_chunk(req, src, src_len)))
     return -1;
-  if (chunk_status)
+  if (final_chunk)
     if (!(ESP_OK == httpd_resp_set_type(req, "application/json") && ESP_OK == httpd_resp_send_chunk(req, src, 0)))
       return -1;
-
   return src_len;
 }
-
 
 static esp_err_t handle_uri_cmd_json(httpd_req_t *req) {
   char buf[256];
