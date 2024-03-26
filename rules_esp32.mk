f299a98b7e79285d4720ea1eefd7204be0978a9d
zwiebert <bertw@online.de>
2024-03-26 18:14:44 +0100

    Improve build files

--- rules_esp32.mk
+++ rules_esp32.mk
@@ -1,11 +1,18 @@
-# add prefix esp32- at command line to make them using esp32/netmcu/Makefile without the esp32-prefix
+# rules related to esp32/netmcu
+
+
+# Targets to forward to sub-directory esp32/netmcu
+#
+# remove prefix esp32- on these targets
 esp32_tgts_auto = doxy-dev-build doxy-usr-build doxy-api-build doxy-dev-view doxy-usr-view doxy-api-view doxy-dev-auto doxy-usr-auto doxy-api-auto \
 		  test.cm.configure test.cm.build test.cm.ctest  test.cm.ctest.regex
-
-# add prefix esp32- at command line to make them using esp32/netmcu/Makefile with the esp32-prefix
+#
+# keep prefix esp32- on these targets
 esp32_tgts_auto2 = menuconfig flash configure all
 
 
+# expand the forwarding rules
+#
 define GEN_RULE
 .PHONY: esp32-$(1)
 esp32-$(1):
--- rules_stm32.mk
+++ rules_stm32.mk
@@ -1,23 +1,31 @@
+# rules related to stm32/rv
+#
 .PHONY: fetch_libopencm3 build_libopencm3
 
+# Make targets to forward to stm32/rv
+#
 #  remove prefix stm32- on these targets
 stm32_tgts_auto = doxy-dev-build doxy-usr-build doxy-api-build doxy-dev-view doxy-usr-view doxy-api-view doxy-usr-auto doxy-api-auto doxa-dev-auto \
                   test.cm.configure test.cm.build test.cm.ctest  test.cm.ctest.regex
-
+#
 # change prefix from stm32- to stm32f1.fw.  on these targets
 stm32_tgts_auto2 = all menuconfig configure build flash
 
 
+# somre rules about fetching and building libopencm3 
+# the Makefine in the sub-directory may handle this by itself
+# 
 pre_build.stm: fetch_libopencm3 build_libopencm3
 build_libopencm3: stm32/rv/external/libopencm3/lib/libopencm3_stm32f1.a
 fetch_libopencm3: stm32/rv/external/libopencm3
-
 stm32/rv/external/libopencm3/lib/libopencm3_stm32f1.a:
 	make -C stm32/rv/external/libopencm3
 stm32/rv/external/libopencm3:
 	git submodule update --init --recursive
 
 
+# expand the forwarding rules
+#
 define GEN_RULE
 .PHONY: stm32-$(1)
 stm32-$(1):
