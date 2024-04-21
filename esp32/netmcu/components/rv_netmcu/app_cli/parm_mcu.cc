4323d4e601dea1e1cce4e35a2e8858f1e2058d29
zwiebert <bertw@online.de>
2024-04-21 22:31:23 +0200

    Improve things

--- esp32/netmcu/components/rv_netmcu/app_cli/parm_mcu.cc
+++ esp32/netmcu/components/rv_netmcu/app_cli/parm_mcu.cc
@@ -2,7 +2,6 @@
 
 #include <string.h>
 
-
 #include "txtio/inout.h"
 #include "app_uout/status_output.h"
 #include "app_misc/rtc.h"
@@ -10,11 +9,13 @@
 #include "cli/cli.h"
 #include "app_cli/cli_app.h"
 #include "stm32/stm32.h"
-#include "stm32/stm32_bl.h"
 #include "stm32/stm32_ota.h"
 #include "app_misc/ota.h"
 #include "net/http_client.h"
 #include "debug/log.h"
+#ifdef MCU_ESP32
+#include <esp_system.h>
+#endif
 #include <app_uout/so_msg.h>
 #include "kvs/kvs_wrapper.h"
 
@@ -26,9 +27,7 @@
 #endif
 #define logtag "rv.cli.mcu"
 
-
 #define KEY_BOOT_COUNT "boot-count"
-
 
 const char cli_help_parmMcu[] = "print=(rtc|cu|reset-info)\n"
 #if ENABLE_SPIFFS
@@ -37,21 +36,29 @@ const char cli_help_parmMcu[] = "print=(rtc|cu|reset-info)\n"
 #ifdef CONFIG_GPIO_SIZE
     "gpioN=(0|1|t|?) clear, set, toggle or read GPIO pin N\n"
 #endif
-        "up-time=?\n"
-        "version=full\n";
-
-
+#ifdef MCU_ESP32
+        "loglevel-get=tag       get loglevel for tag\n"
+        "loglevel-none=tag      set loglevel for tag\n"
+        "loglevel-error=tag     set loglevel for tag\n"
+        "loglevel-warn=tag      set loglevel for tag\n"
+        "loglevel-info=tag      set loglevel for tag\n"
+        "loglevel-debug=tag     set loglevel for tag\n"
+        "loglevel-verbose=tag   set loglevel for tag\n"
+#endif
+#ifdef DISTRIBUTION
+    "stm32ota=(github-master|github-beta)      Do OTA firmware update for connected STM32\n"
+#else
+    "stm32ota=(github-master|github-beta|URL)  Do OTA firmware update for connected STM32\n"
+#endif
+    "up-time=?\n"
+    "version=full\n";
 
 static void kvs_print_keys(const char *name_space);
 
-
-int
-process_parmMcu(clpar p[], int len, class UoutWriter &td) {
+int process_parmMcu(clpar p[], int len, class UoutWriter &td) {
   int arg_idx;
-  int error_count = 0;
 
   soMsg_MCU_begin(td);
-
 
   for (arg_idx = 1; arg_idx < len; ++arg_idx) {
     const char *key = p[arg_idx].key, *val = p[arg_idx].val;
@@ -61,33 +68,19 @@ process_parmMcu(clpar p[], int len, class UoutWriter &td) {
     } else if (strcmp(key, KEY_BOOT_COUNT) == 0 && *val == '?') {
       soMsg_MCU_BOOT_COUNT(td);
     } else if (strcmp(key, "rbl") == 0) {
-       db_logi(logtag, "run bootloader");
-       stm32_runBootLoader();
+      db_logi(logtag, "run bootloader");
+      stm32_runBootLoader();
+    } else if (strcmp(key, "rfw") == 0) {
+      db_logi(logtag, "run firmware");
+      stm32_runFirmware();
+#if 0 // these boot-loader functions are now inaccessible in private includes
     } else if (strcmp(key, "blstart") == 0) {
       stm32Bl_doStart();
     } else if (strcmp(key, "blgid") == 0) {
       stm32Bl_getId();
     } else if (strcmp(key, "blget") == 0) {
       stm32Bl_get();
-    } else if (strcmp(key, "rfw") == 0) {
-       db_logi(logtag, "run firmware");
-       stm32_runFirmware();
-    } else if (strcmp(key, "dlrvbin") == 0) {
-      db_logi(logtag, "download rv.bin");
-      stm32Ota_firmwareDownload(val, STM32_FW_FILE_NAME);
-      // mcu dlrvbin=http://192.168.1.70:8000/rv.bin;
-    } else if (strcmp(key, "rvota") == 0) {
-      db_logi(logtag, "download rv.bin");
-      if (stm32Ota_firmwareDownload(val, STM32_FW_FILE_NAME)) {
-        if (stm32Ota_firmwareUpdate(STM32_FW_FILE_NAME)) {
-        } else {
-          ++error_count;
-        }
-      } else {
-        ++error_count;
-      }
-    } else if (strcmp(key, "rvfl") == 0) {
-      stm32Ota_firmwareUpdate(STM32_FW_FILE_NAME);
+#endif
 #ifdef USE_STM32OTA
     } else if (strcmp(key, "stm32ota") == 0) {
       if (*val == '?') {
@@ -130,16 +123,12 @@ process_parmMcu(clpar p[], int len, class UoutWriter &td) {
 #endif
       }
 #endif
-    } else if (strcmp(key, "flrvbin") == 0) {
-       db_logi(logtag, "flash rv.bin");
-       stm32Bl_writeMemoryFromBinFile("/spiffs/rv.bin", 0x8000000);
     } else if (strcmp(key, "flrv") == 0) {
       stm32Ota_firmwareUpdate(STM32_FW_FILE_NAME);
 
     } else if (strcmp(key, "kvs-pk") == 0) {
-        kvs_print_keys(val);
-        break;
-
+      kvs_print_keys(val);
+      break;
 
 #ifdef ACCESS_GPIO
     } else if (strncmp(key, "gpio", 4) == 0) {
@@ -186,7 +175,6 @@ process_parmMcu(clpar p[], int len, class UoutWriter &td) {
       }
 #endif
 
-
     } else if (strcmp(key, "up-time") == 0) {
       if (*val == '?') {
         soMsg_MCU_RUN_TIME(td);
@@ -196,6 +184,33 @@ process_parmMcu(clpar p[], int len, class UoutWriter &td) {
 
     } else if (strcmp(key, "version") == 0) {
       soMsg_MCU_VERSION(td);
+#ifdef MCU_ESP32
+    } else if (strcmp(key, "loglevel-get") == 0) {
+      td.so().print("loglevel", (int) esp_log_level_get(val));
+    } else if (strcmp(key, "loglevel-none") == 0) {
+      esp_log_level_set(val, ESP_LOG_NONE);
+      td.so().print("loglevel", (int) esp_log_level_get(val));
+    } else if (strcmp(key, "loglevel-error") == 0) {
+      esp_log_level_set(val, ESP_LOG_ERROR);
+      td.so().print("loglevel", (int) esp_log_level_get(val));
+    } else if (strcmp(key, "loglevel-warn") == 0) {
+      esp_log_level_set(val, ESP_LOG_WARN);
+      td.so().print("loglevel", (int) esp_log_level_get(val));
+    } else if (strcmp(key, "loglevel-info") == 0) {
+      esp_log_level_set(val, ESP_LOG_INFO);
+      td.so().print("loglevel", (int) esp_log_level_get(val));
+    } else if (strcmp(key, "loglevel-debug") == 0) {
+      esp_log_level_set(val, ESP_LOG_DEBUG);
+      td.so().print("loglevel", (int) esp_log_level_get(val));
+    } else if (strcmp(key, "loglevel-verbose") == 0) {
+      esp_log_level_set(val, ESP_LOG_VERBOSE);
+      td.so().print("loglevel", (int) esp_log_level_get(val));
+    } else if (strcmp(key, "free-heap-size") == 0) {
+      if (*val == '?') {
+        const uint32_t fhs = esp_get_free_heap_size();
+        td.so().print("free-heap-size", fhs);
+      }
+#endif
     } else {
       cli_warning_optionUnknown(td, key);
     }
@@ -206,7 +221,6 @@ process_parmMcu(clpar p[], int len, class UoutWriter &td) {
 
   return 0;
 }
-
 
 static void kvs_print_keys(const char *name_space) {
   kvs_foreach(name_space, KVS_TYPE_ANY,
